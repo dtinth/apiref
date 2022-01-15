183f3fc989e0af5315e68c5f6762b45756d1f59f
dtinth on MBP M1 <dtinth@spacet.me>
2022-01-16 01:25:28 +0700

    try

--- api/index.js
+++ api/index.js
@@ -1,8 +1,6 @@
 const { createRequestHandler } = require('@remix-run/vercel')
 
-// ???
 require('@microsoft/tsdoc')
-require('@microsoft/api-extractor-model')
 
 module.exports = createRequestHandler({
   build: require('./_build'),
