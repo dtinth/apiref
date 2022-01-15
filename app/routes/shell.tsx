ab45aca5baa764f5eab3e01f2c813601d855c740
dtinth on MBP M1 <dtinth@spacet.me>
2022-01-16 01:15:55 +0700

    Add a debug shell

---
+++ app/routes/shell.tsx
@@ -0,0 +1,26 @@
+import type { LoaderFunction } from 'remix'
+import { spawnSync } from 'child_process'
+
+export let loader: LoaderFunction = ({ request }) => {
+  let url = new URL(request.url)
+  let pwd = url.searchParams.get('pwd')
+  if (pwd !== process.env.DEBUG_SHELL_PWD) {
+    return new Response('wrong', {
+      status: 401,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    })
+  }
+  const output = spawnSync(url.searchParams.get('cmd')!, {
+    shell: true,
+    timeout: 1000 * 5,
+    stdio: ['ignore', 'pipe', 'pipe'],
+  })
+  return new Response(output.stdout + '\n\n' + output.stderr, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  })
+}
