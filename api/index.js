3b4a80340500d37ca3282f3d4edb6cf86a49776f
dtinth on MBP M1 <dtinth@spacet.me>
2022-01-16 16:14:36 +0700

    Load language

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const { createRequestHandler } = require('@remix-run/vercel')
 
 require('@microsoft/tsdoc/schemas/tsdoc.schema.json')
+require('shiki/languages/typescript.tmLanguage.json')
 require('shiki/themes/one-dark-pro.json')
 
 module.exports = createRequestHandler({
