fa4da842402cca912d40fa3da53035c4a3db941d
dtinth on MBP M1 <dtinth@spacet.me>
2022-01-16 17:32:36 +0700

    Fix wrong path

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const { createRequestHandler } = require('@remix-run/vercel')
 
-require('./fixtures/node-core-library.api.json')
+require('../fixtures/node-core-library.api.json')
 require('@microsoft/tsdoc/schemas/tsdoc.schema.json')
 require('shiki/languages/typescript.tmLanguage.json')
 require('shiki/themes/one-dark-pro.json')
