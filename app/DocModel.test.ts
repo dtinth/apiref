6b902b7dfda230e476521f40fc78c66b3c7b9c52
dtinth on MBP M1 <dtinth@spacet.me>
2022-01-18 01:02:46 +0700

    Fix test

--- app/DocModel.test.ts
+++ app/DocModel.test.ts
@@ -1,7 +1,11 @@
 import { expect, test } from 'vitest'
+import { Diagnostic } from './DiagnosticWriter'
 import { getApiModel } from './DocModel.server'
 
 test('ee', async () => {
-  const model = await getApiModel('_fixture')
+  const model = await getApiModel(
+    'fixtures:node-core-library',
+    new Diagnostic(),
+  )
   expect(model).toBeTruthy()
 })
